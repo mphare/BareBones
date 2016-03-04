de2d8054115815577c1d2f81fc1895e875c7c34e
Michael hare <mhare@CentOS7.local>
2016-03-04 16:30:03 -0600

    Now that Go CD seems to be working

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -2,7 +2,7 @@ package oata;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Updated World!");
+        System.out.println("Hello Go CD, It's an Updated World!");
     }
 }
 
